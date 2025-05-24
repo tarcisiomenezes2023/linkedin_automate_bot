c6ca7ac7701fcdc350af37ebb0648395028a49c9
Tarcísio <tarcisiomenezes2019@hotmail.com>
2025-05-25 00:09:32 +0200

    3° commit

---
+++ training.py
@@ -0,0 +1,9 @@
+# Browser setup
+import undetected_chromedriver as uc
+from selenium.webdriver.chrome.options import Options
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+import time
+import random
+
+def 
