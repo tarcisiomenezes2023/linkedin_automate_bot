08115e6e9c1ea3245933de4945a71eba4b9b7281
Tarcísio <tarcisiomenezes2019@hotmail.com>
2025-06-03 00:40:35 +0200

    needing to fix job filterings

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #main.py
 from src.Login.login import driver
 from src.Feed.feed import simulate_human_behavior
+from src.Filter.filter import filter_jobs
 
 # Driver automated on the feed
 simulate_human_behavior(driver)
 
-#Progress
-print("Now it's ready to move to Job application section")
+# Job Application
+filter_jobs(driver)
