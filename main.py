2acb7400a80e5572fe24e937bf3def52bd816b16
Tarcísio <tarcisiomenezes2019@hotmail.com>
2025-06-03 21:11:10 +0200

    Filters done!

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #main.py
 from src.Login.login import driver
 from src.Feed.feed import simulate_human_behavior
-from src.Filter.filter import filter_jobs
+from src.Filter.remote_filter import Remote_filter_jobs
 
 # Driver automated on the feed
 simulate_human_behavior(driver)
 
 # Job Application
-filter_jobs(driver)
+Remote_filter_jobs(driver)
---
+++ src/Filter/remote_filter.py
@@ -0,0 +1,107 @@
+import time
+import random
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def Remote_filter_jobs(driver):
+    job_keywords = "Python" # "Excel", "programming", "frontend", "Javascript", "data", "sustainability", "environmental", "water", "air", "health and safety
+    location_keyword = "European Union" 
+
+    wait = WebDriverWait(driver, 10)
+
+    try:
+        # Job and location input
+        job_field = wait.until(EC.element_to_be_clickable((By.XPATH, '//input[contains(@id, "jobs-search-box-keyword-id")]')))
+        location_field = wait.until(EC.element_to_be_clickable((By.XPATH, '//input[contains(@id, "jobs-search-box-location-id")]')))
+
+        job_field.clear()
+        location_field.clear()
+
+        job_field.send_keys(job_keywords)
+        time.sleep(random.uniform(1.0, 5.0))
+        location_field.send_keys(Keys.CONTROL + "a")
+        location_field.send_keys(Keys.BACKSPACE)
+        time.sleep(random.uniform(1.0, 5.0))
+
+        job_field.send_keys(Keys.RETURN)
+        time.sleep(random.uniform(3.0, 5.0))
+
+    except Exception as e:
+        print(f'❌ Error entering search terms: {e}')
+
+    # === Filter: All filters button ===
+    try:
+        # Wait and locate the "All filters" button by its ID
+        all_filters_btn = WebDriverWait(driver, 15).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Show all filters. Clicking this button displays all available filter options.']"))
+        )
+        # Scroll into view and click
+        driver.execute_script("arguments[0].scrollIntoView(true);", all_filters_btn)
+        time.sleep(random.uniform(1.0, 3.0))
+        all_filters_btn.click()
+        print("✅ 'All filters' button clicked successfully!")
+        time.sleep(random.uniform(1.0, 8.0))
+
+    except Exception as e:
+        print(f"⚠️ Could not apply 'All filters' filter: {e}")
+        
+    # === Filterings for Most recent, Past 24 hours, Internship and Entry level, and Remote.
+    # === Date posted: Past 24 hours ===
+    try:
+        date_posted_option = wait.until(EC.element_to_be_clickable((
+        By.XPATH, "//label[contains(., 'Past 24 hours')]"
+        )))
+        date_posted_option.click()
+        print("✅ 'Past 24 hours' option selected!")
+
+    except Exception as e:
+        print(f"⚠️ Could not select 'Past 24 hours': {e}")
+
+
+    # === Experience Level: Internship and Entry level ===
+    try:
+        internship = wait.until(EC.element_to_be_clickable((
+        By.XPATH, "//label[contains(., 'Internship')]"
+        )))
+        internship.click()
+        time.sleep(random.uniform(1.0, 5.0))
+
+        entry = wait.until(EC.element_to_be_clickable((
+        By.XPATH, "//label[contains(., 'Entry level')]"
+        )))
+        entry.click()
+        print("✅ 'Internship' and 'Entry level' options selected!")
+
+    except Exception as e:
+        print(f"⚠️ Could not select 'Internship and Entry level': {e}")
+
+
+    # === Remote ===
+    try:
+        remote_option = wait.until(EC.element_to_be_clickable((
+        By.XPATH, "//label[contains(., 'Remote')]"
+        )))
+        remote_option.click()
+        print("✅ 'Remote' option selected!")
+
+    except Exception as e:
+        print(f"⚠️ Could not select 'Remote': {e}")
+
+    # Apply filters:
+    try:
+        show_results_button = wait.until(EC.element_to_be_clickable((
+        By.XPATH, "//button[contains(@class, 'search-reusables__secondary-filters-show-results-button') and contains(., 'Show')]"
+        )))
+
+        # Scroll into view and click via JS to avoid interception
+        driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", show_results_button)
+        time.sleep(random.uniform(0.5, 5.0))
+        driver.execute_script("arguments[0].click();", show_results_button)
+
+        print("✅ 'Show results' clicked!")
+        time.sleep(random.uniform(1.0, 5.0))
+
+    except Exception as e:
+        print(f"⚠️ Could not click 'Show results': {e}")
